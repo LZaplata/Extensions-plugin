3abdf5141b837e9ae40031063e44f72f00262a6c
Lukáš Záplata <lukas.zaplata@gmail.com>
2025-03-14 10:26:32 +0100

    Small GDPR data seeder change

---
+++ updates/rainlab_blog_settings.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace LZaplata\Extensions\Updates;
+
+use Illuminate\Support\Facades\DB;
+use October\Rain\Database\Updates\Seeder;
+
+class RainlabBlogSettings extends Seeder
+{
+    public function run()
+    {
+        $value = file_get_contents(__DIR__ . "/../seeds/data/rainlab-blog-settings.json");
+
+        DB::table("system_settings")->updateOrInsert([
+            "item" => "rainlab_blog_settings"
+        ], [
+            "value" => json_encode(json_decode($value)),
+        ]);
+    }
+}
